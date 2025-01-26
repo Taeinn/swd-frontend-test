91db78fd85d543f347db66d45f05f1a6ff81c114
Taeinn <86055887+Taeinn@users.noreply.github.com>
2025-01-27 00:08:41 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 // import { i18n } from 'next-i18next';
 
+// import { locale } from "dayjs";
+
 const nextConfig = {
   i18n: {
     locales: ['en', 'th'],  // Define your supported languages
     defaultLocale: 'en',     // Set your default language
+    localeDetection: false
   },
 };
 
